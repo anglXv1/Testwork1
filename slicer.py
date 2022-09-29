2a2a61a6a471315ac436a0305b1cf0b810cd51e1
anglXv1 <113514444+anglXv1@users.noreply.github.com>
2022-09-29 12:42:50 +0300

    Add files via upload

--- slicer.py
+++ slicer.py
@@ -1,3 +1,5 @@
 if __name__ == "__main__":
-    text = "y3tds23012345dujderpvpasdtgxaefs3gss5432fjs24fsswe098765d"
-    return = 'Youpassed'
+    text = "y3tds23o12345dujderpvpasdtgxaefs3gss5432fjs24fsswe098765d".capitalize()
+    current_slice = text[::7]
+
+    print(current_slice)
